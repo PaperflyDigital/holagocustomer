689b9bee34a86681acc74748333172f2f15ca0c1
shaiadul <devshaiadul69@gmail.com>
2024-07-11 22:58:12 +0600

    vocab

--- components/pageSection/HomePage/Refresh.jsx
+++ components/pageSection/HomePage/Refresh.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Refresh() {
   return (
     <section className="container mx-auto">
-      <div className="bg-[#F4F4F4] rounded-md shadow-md my-10 px-10">
+      <div className="bg-[#F4F4F4] rounded-md shadow-md mx-10 my-20 px-10">
         <div className="flex justify-between items-center">
           <div>
             <Image src={refPic} alt="refresh" />
