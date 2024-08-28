30febd95f7b73cbaff60fce1fac6a2da5e60a23a
Safayat <safayatofficial1@gmail.com>
2024-08-28 09:46:54 +0600

    redux added

--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -1,29 +1,92 @@
+"use client"
 import Button from '@/components/global/Button'
 import CheckBoxInput from '@/components/global/CheckBoxInput'
+import OtpInput from '@/components/global/OtpInput'
 import TextInput from '@/components/global/TextInput'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 
+
+const validateContact = (contact) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const sanitizedContact = contact.replace(/\D/g, '');
+    const phoneRegex = /^\d{7,15}$/;
+    if (emailRegex.test(contact)) return { type: "email", valid: true };
+    else if (phoneRegex.test(sanitizedContact)) return { type: "phone", valid: true, sanitizedContact };
+    else return { type: "invalid", valid: false };
+};
 const page = () => {
+    const [step, setStep] = useState(0)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { contact, password, rPassword, name } = e.target.elements;
+        const data = {
+            name: name.value,
+            password: "",
+            email: "",
+            phone: "",
+            role: "CUS"
+        };
+        const validationResult = validateContact(contact.value);
+        if (validationResult.valid) {
+            if (validationResult.type === 'email') {
+                data.email = contact.value;
+            } else if (validationResult.type === 'phone') {
+                data.phone = contact.value.replace(/\D/g, '');
+            }
+        } else {
+            return toast.error("Invalid email or phone number");
+        }
+        if (password.value !== rPassword.value) {
+            return toast.error("Passwords don't match");
+        }
+        data.password = password.value;
+        console.log(data);
+        setStep(1)
+    };
+    const handleOtpVerify = (e) => {
+        const otp = e.target.otp.value;
+
+    }
     return (
         <div >
-            <p className='text-xl font-semibold text-center'>Create your account</p>
-            <div className='w-full mt-5 space-y-3'>
-                <TextInput placeholder={'Email Address or Phone Mumber'}></TextInput>
-                <TextInput placeholder={'Enter Password'}></TextInput>
-                <TextInput placeholder={'Re-Enter Password'}></TextInput>
-                <TextInput placeholder={'Full Name'}></TextInput>
-                <div className='flex justify-between items-center'>
-                    <CheckBoxInput label={'Remember me'} id={'remC'} />
-                </div>
-                <div className='mt-2 space-y-2'>
-                    <Button variant='primary' className={'w-full'}>Sign up</Button>
-                </div>
-                <div className='flex items-center gap-1 pt-5 justify-center'>
-                    Have an account?
-                    <Link href={''} className='text-blue font-medium'>Login here</Link>
-                </div>
-            </div>
+            {
+                step === 0 ? <div>
+                    <p className='text-xl font-semibold text-center'>Create your account</p>
+                    <form onSubmit={handleSubmit} className='w-full mt-5 space-y-3'>
+                        <TextInput placeholder={'Email Address or Phone Mumber'} name={'contact'}></TextInput>
+                        <TextInput placeholder={'Enter Password'} name={'password'}></TextInput>
+                        <TextInput placeholder={'Re-Enter Password'} name={'rPassword'}></TextInput>
+                        <TextInput placeholder={'Full Name'} name={'name'}></TextInput>
+                        <div className='flex justify-between items-center'>
+                            <CheckBoxInput label={'Remember me'} id={'remC'} />
+                        </div>
+                        <div className='mt-2 space-y-2'>
+                            <Button variant='primary' className={'w-full'}>Sign up</Button>
+                        </div>
+                        <div className='flex items-center gap-1 pt-5 justify-center'>
+                            Have an account?
+                            <Link href={''} className='text-blue font-medium'>Login here</Link>
+                        </div>
+                    </form>
+                </div> : step === 1 && <form onSubmit={handleOtpVerify}>
+                    <p className='text-xl text-center font-semibold'>Let’s verify your identity</p>
+                    <p className='text-sm font-medium text-center'>Enter the 4-digit code you received</p>
+                    <div className='my-5'>
+                        <OtpInput name="otp" />
+                    </div>
+                    <Button className={'w-full mt-3'} >Verify</Button>
+                    <div className='flex justify-center'>
+                        <button className='underline text-center mt-2'>Resend Code</button>
+                    </div>
+                    <div className='flex items-center gap-1 pt-5 justify-center'>
+                        Still need help?
+                        <Link href={''} className='text-blue font-medium'>Contact us</Link>
+                    </div>
+                </form>
+            }
+
         </div>
     )
 }
---
+++ utils/functions.js
@@ -0,0 +1,58 @@
+import { setAuth } from "@/redux/slices/AuthSlice";
+import { useSelector } from "react-redux";
+import { FetchApi } from "./FetchApi";
+
+export const refetchAuthState = async (auth) => {
+    try {
+        const res = await FetchApi({
+            url: `/user/getUserById/${auth?.user._id}`, callback: () => {
+            }
+        })
+        if (res.status === 200) {
+            const newObj = {
+                ...auth,
+                user: res.data.user,
+            }
+            if (!res.data.user.isActive) {
+                store.dispatch(setAuth({}))
+                return window.location = '/login'
+            }
+            store.dispatch(setAuth(newObj))
+        }
+    } catch (error) {
+        store.dispatch(setAuth({}))
+        window.location = '/login'
+    }
+}
+
+export const useAuth = () => {
+    const auth = useSelector((state) => state.auth?.user);
+    return {
+        auth: auth?.user,
+        refetchAuth: () => refetchAuthState(auth),
+        token: auth?.token
+    }
+}
+export const loginUser = async (email, password) => {
+    try {
+      const res = await FetchApi({ url: '/admin/signInAllUsers', method: 'post', data: { email, password }, })
+      if (res?.status === 200) {
+        if (res?.data?.user?.role === 'MP') {
+          store.dispatch(setAuth(res?.data))
+          window.location = '/dashboard'
+        } else {
+          toast.error('Invalid email or password.')
+        }
+      }
+  
+      return res;
+    } catch (error) {
+      toast.error(error.response.data.message || error.message)
+      console.error(error)
+    }
+  }
+export const logoutUser = () => {
+    store.dispatch(setAuth({}))
+    window.location = '/login'
+  
+  }
